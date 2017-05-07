c5a52a1b75f834e8b70d604501c86cf3ea730ed6
Anastasia Marie <ana.goodwin10@gmail.com>
2017-05-07 14:47:04 -0500

    updates

--- js/logic.js
+++ js/logic.js
@@ -9,29 +9,16 @@ $(document).ready(function() {
     "use strict"; // Start of use strict
 
     // Collapse the navbar when page is scrolled
-    $(window).scroll(function() {
-        if ($("#mainNav").offset().top > 100) {
-            $("#mainNav").addClass("navbar-shrink");
-        } else {
-            $("#mainNav").removeClass("navbar-shrink");
-        }
-    });
-
-    // Activate scrollspy to add active class to navbar items on scroll
-    $('body').scrollspy({
-        target: '#mainNav',
-        offset: 68
-    });
-
-    // Smooth Scrolling: Smooth scrolls to an ID on the current page
-    // To use this feature, add a link on your page that links to an ID, and add the .page-scroll class to the link itself. See the docs for more details.
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 68)
-        }, 1250, 'easeInOutExpo');
+  $('a[href^="#"]').on('click', function(event) {
+    var target = $(this.getAttribute('href'));
+    if( target.length ) {
         event.preventDefault();
-    });
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 2000);
+    }
+});
     
+});
 });
 
